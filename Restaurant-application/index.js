b9d77aacbdac7a4f399d0dc0cb6310b21895ff30
Vignesh S <vignesh.s@crystalcodelabs.com>
2026-04-17 17:56:26 +0530

    feat: added the header component.

--- Restaurant-application/index.js
+++ Restaurant-application/index.js
@@ -1,22 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-
+import ReactDOM from "react-dom/client";
 
 const Header = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div>
+        <img
+          src="https://logowik.com/content/uploads/images/swiggy2340.jpg"
+          className="logo"
+        />
+      </div>
 
-const RestaurantApplication = () =>{
-    return(
-        <Header/>
-    )
-}
+      <div>
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
+const RestaurantApplication = () => {
+  return <Header />;
+};
 
-const root = React.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RestaurantApplication/>);
+root.render(<RestaurantApplication />);
