633e740a93e620e392fac33be402a3c0d71ac8b0
Vignesh S <vignesh.s@crystalcodelabs.com>
2026-04-17 10:58:23 +0530

    feat: added the react first program

---
+++ React-first-program/header-using-react.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  {},
+  "This is a heading rendering from react!",
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
