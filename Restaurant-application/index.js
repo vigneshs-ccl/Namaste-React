82f73272609109a61553dc2519dc023d91eb3d88
Vignesh S <vignesh.s@crystalcodelabs.com>
2026-04-17 17:13:54 +0530

    feat: created restaurant application setup.

---
+++ Restaurant-application/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const Header = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+const RestaurantApplication = () =>{
+    return(
+        <Header/>
+    )
+}
+
+
+const root = React.createRoot(document.getElementById("root"))
+
+root.render(<RestaurantApplication/>);
